a4c0062fa43afe3731d6bcf5dabe6bfdc89b095a
eiriksm <eirik@morland.no>
2015-05-15 17:17:54 +0200

    Fix minor typo (usre -> sure)

--- netsniff.js
+++ netsniff.js
@@ -13,7 +13,7 @@
  * Email: iroy2000 [at] gmail.com
  */
 
-// make usre toISOString is available in Date object
+// make sure toISOString is available in Date object
 if (!Date.prototype.toISOString) {
     Date.prototype.toISOString = function () {
         function pad(n) { return n < 10 ? '0' + n : n; }
